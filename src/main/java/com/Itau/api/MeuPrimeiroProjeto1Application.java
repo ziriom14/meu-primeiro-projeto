e8cd779e25827da26a30fb866751d0384f81ef59
ziriom14 <ziriom14@gmail.com>
2020-01-27 17:05:01 -0300

    Versao inicial do projeto..

---
+++ src/main/java/com/Itau/api/MeuPrimeiroProjeto1Application.java
@@ -0,0 +1,13 @@
+package com.Itau.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjeto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjeto1Application.class, args);
+	}
+
+}
